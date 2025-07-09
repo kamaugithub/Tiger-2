8c9e2ce9af7998e9e740174632eb0d2f689d01f7
Mathew Ramon <ramonmatkax6@gmail.com>
2025-07-09 12:51:07 +0300

    Updated navbar responsiveness and scroll styling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('scrolled', window.scrollY > 50);
   });
-  
 
 // Mobile menu toggle
 const toggle = document.getElementById('menu-toggle');
